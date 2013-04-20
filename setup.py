7442364960b8debb67f7103d040d67496c14af96
Mike Patek <mpatek@gmail.com>
2013-04-20 14:42:49 -0400

    packaging with distutils.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='py-ssh-alias',
+    version='1.0',
+    description='Script for managing SSH aliases',
+    author='Mike Patek',
+    author_email='mpatek@gmail.com',
+    url='https://github.com/mpatek/py-ssh-alias',
+    scripts=['ssh_alias.py'],
+)
